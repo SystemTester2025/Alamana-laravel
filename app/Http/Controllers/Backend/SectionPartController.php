7433dabbec04bfd4334010a38f21b1c073757a9c
SystemTester2025 <ahmedmagdysystem2025@gmail.com>
2025-03-22 14:53:20 +0200

    Refactor image upload logic in SectionPartController to improve file handling. Enhanced the image upload process by ensuring the old image is deleted only if it exists before uploading a new one. This change streamlines the image management process and prevents potential file system errors, contributing to better content management and user experience.

--- app/Http/Controllers/Backend/SectionPartController.php
+++ app/Http/Controllers/Backend/SectionPartController.php
@@ -105,13 +105,16 @@ class SectionPartController extends Controller
         $oldAttributes = $sectionPart->toArray();
         $oldSectionId = $sectionPart->section_id;
         
-        if ($request->hasFile('image') && $sectionPart->image) {
-            unlink(public_path($sectionPart->image));
+        if ($request->hasFile('image')) {
+            // Delete old logo if exists
+            if ($sectionPart->image && file_exists(public_path($sectionPart->image))) {
+                unlink(public_path($sectionPart->image));
+            }
+            $imagePath = 'images/uploads';
+            $imageName = time() . '_' . rand(1000, 9999) . '_' . $request->file('favicon')->getClientOriginalName();
+            $request->file('image')->move(public_path($imagePath), $imageName);
+            $sectionPart->image = $imagePath . '/' . $imageName;
         }
-        $imagePath = 'images/uploads';
-        $imageName = time() . '_' . rand(1000, 9999) . '_' . $request->file('image')->getClientOriginalName();
-        $request->file('image')->move(public_path($imagePath), $imageName);
-        $data['image'] = $imagePath . '/' . $imageName;
 
         
         $sectionPart->update($data);
